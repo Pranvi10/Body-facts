cd797ee105e68f86b5762b1d88444ada227f1d76
Pranvi <75794298+Pranvi10@users.noreply.github.com>
2021-01-26 14:56:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,123 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background("white");
+  
+  fill("black");
+  stroke(65, 135, 232);
+  textSize(30);
+  text("Interesting Facts about our body.",200,40);
+  
+  fill("red");
+  stroke(8);
+  textSize(15);
+  text("Hi! I am Pranvi. Today I would like to tell about some interesting facts about our body.",10,70);
+  
+  fill("yellow");
+  stroke(8);
+  textSize(15);
+  text("So let's get started!",10,90);
+  
+  fill("green");
+  stroke(8);
+  textSize(15);
+  text("1. Our fingernails grow four times as fast as our toenails",10,110);
+  
+  fill("pink");
+  stroke(8);
+  textSize(15);
+  text("2. Babies are born with 300 bones – adults have 206",10,130);
+  
+  fill("orange");
+  stroke(8);
+  textSize(15);
+  text("3. Eyelashes last about 150 days",10,150);
+  
+  fill("purple");
+  stroke(8);
+  textSize(15);
+  text("4. The heart circulates our blood in our body about 1,000 times each day",10,170); 
+  
+  fill("black");
+  stroke(8);
+  textSize(15);
+  text("5. Our body make about half a quart (500ml) of spit each day",10,190);
+  
+  fill("brown");
+  stroke(8);
+  textSize(15);
+  text("6. The smallest bone in our body is in our ears",10,210);
+  
+  fill("blue");
+  stroke(8);
+  textSize(15);
+  text("7. Our blood has the same amount of salt in it as the ocean does",10,230);
+  
+  fill("red");
+  stroke(8);
+  textSize(15);
+  text("8. A sneeze blows air out of our nose at 100 miles per hour",10,250);
+  
+  fill("yellow");
+  stroke(8);
+  textSize(15);
+  text("9. We are taller in the morning than we are at night",10,270);  
+  
+  fill("green");
+  stroke(8);
+  textSize(15);
+  text("10. Humans are the one that cry when they are upset",10,290);
+  
+  fill("pink");
+  stroke(8);
+  textSize(15);
+  text("11. Many more boys are color blind than girls are",10,310);
+  
+  fill("orange");
+  stroke(8);
+  textSize(15);
+  text("12. Children have more taste buds than adults",10,330);  
+  
+  fill("purple");
+  stroke(8);
+  textSize(15);
+  text("13. Our lungs are the only organs in our body that float",10,350);  
+  
+  fill("black");
+  stroke(8);
+  textSize(15);
+  text("14. Food spends up to 6 hours in the stomach being digested",10,370);  
+  
+  fill("brown");
+  stroke(8);
+  textSize(15);
+  text("15. Our eyeballs are actually part of our brain",10,390);  
+  
+  fill("blue")
+  stroke(8);
+  textSize(15);
+  text("16. Bones are 4 times stronger than concrete",10,410);  
+  
+  fill("red");
+  stroke(8);
+  textSize(15);
+  text("17. The largest muscle in our body is the one we are sitting on!",10,430);
+  
+  fill("yellow");
+  stroke(8);
+  textSize(15);
+  text("18. Every square inch of the human body has about 19 million skin cells",10,450);  
+  
+  fill("green");
+  stroke(8);
+  textSize(15);
+  text("19. Every hour about 1 billion cells in the human body must be replaced",10,470);  
+  
+  fill("purple");
+  stroke(8);
+  textSize(15);
+  text("20. The average human head has about 100,000 hairs",10,490); 
+
+}
